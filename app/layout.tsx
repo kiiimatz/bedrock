0a64c6b346ccb0273872c58cce3cc06cbcc981fd
kiiimatz <kiiimatsy@gmail.com>
2026-06-11 23:23:35 +0900

    Change page title to Bedrock Status

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'bedrock — status',
+  title: 'Bedrock Status',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
